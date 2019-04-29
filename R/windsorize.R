3fae72a0ab8f72c69f93798fe200656bc8300063
Tibor Szabo <tibor.szabo1988@gmail.com>
2019-04-29 20:35:23 +0000

    Fix issue #2: Function windsorize throws error if the input contains NA or is NULL

--- R/windsorize.R
+++ R/windsorize.R
@@ -3,6 +3,14 @@
 #' Do some windsorization.
 #' @export
 windsorize <- function(x, p = .90) {
+  
+  if( sum(is.na(x))){
+    stop("Input x contains value NA")
+  }
+  else if( is.null(x) ){
+    stop("Input x is NULL")
+  }
+  
   q_lower <- quantile(x, (1-p)/2)
   q_upper <- quantile(x, p + (1-p)/2)
   x[x <= q_lower] <- q_lower
