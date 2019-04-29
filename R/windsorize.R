9431e331774e54388a7a2373b7c34f3af473699f
Tibor Szabo <tibor.szabo1988@gmail.com>
2019-04-29 20:20:10 +0000

    Fix issue #1: Function windsorize() replaces correctly values below and above the specified percentile

--- R/windsorize.R
+++ R/windsorize.R
@@ -3,8 +3,10 @@
 #' Do some windsorization.
 #' @export
 windsorize <- function(x, p = .90) {
-  q <- quantile(x, p)
-  x[x >= q] <- q
+  q_lower <- quantile(x, (1-p)/2)
+  q_upper <- quantile(x, p + (1-p)/2)
+  x[x <= q_lower] <- q_lower
+  x[x >= q_upper] <- q_upper
   x
 }
 
