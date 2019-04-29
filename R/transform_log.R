1a15e0fab3afb6cf59e687510446ccd24fe82b21
Tibor Szabo <tibor.szabo1988@gmail.com>
2019-04-29 21:16:58 +0000

    Fix issue #6: Function documentations are added. Fix issue #7: Handling NULL values corrected

--- R/transform_log.R
+++ R/transform_log.R
@@ -1,9 +1,20 @@
+#' Transform_log
+#'
+#' Transforms values to natural logarithm.
+#' 
+#' @param x A numeric vector containing positive real numbers
+#' @return Natural logarithm of the values in \code{x}.
+#' @examples
+#' transform_log(x = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+#' transform_log(c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+#' @export
+
 transform_log <- function(x){
   
   if( sum(is.na(x)) ){
     stop("Input x contains value NA")
   }
-  else if( length(x) ){
+  else if( length(x) == 0 ){
     stop("Input x is NULL")
   }  
   else if( sum(x <= 0)  ){
--- R/windsorize.R
+++ R/windsorize.R
@@ -1,13 +1,22 @@
 #' Windsorize
 #'
 #' Do some windsorization.
+#' 
+#' @param x A numeric vector containing real numbers
+#' @param p A number between 0 and 1, representing the mass of probability. Values are winsorized outside \code{p}. Default value is 0.9. 
+#' @return Windsorized vector of \code{x}.
+#' @examples
+#' windsorize(x = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+#' windsorize(x = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), p = 0.8)
+#' windsorize(c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 0.8)
 #' @export
+
 windsorize <- function(x, p = .90) {
   
   if( sum(is.na(x))){
     stop("Input x contains value NA")
   }
-  else if( length(x) ){
+  else if( length(x) == 0 ){
     stop("Input x is NULL")
   }
   
