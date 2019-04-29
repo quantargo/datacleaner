84bf9e5e38fe4ab511b637034fd276dac63ade6b
Tibor Szabo <tibor.szabo1988@gmail.com>
2019-04-29 20:49:08 +0000

    Fix issue #5: Typo is corrected in the code

--- R/transform_log.R
+++ R/transform_log.R
@@ -3,7 +3,7 @@ transform_log <- function(x){
   if( sum(is.na(x)) ){
     stop("Input x contains value NA")
   }
-  else if( length(x) |  ){
+  else if( length(x) ){
     stop("Input x is NULL")
   }  
   else if( sum(x <= 0)  ){
