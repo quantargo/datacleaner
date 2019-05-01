5782aa0b23c1e46cbc372de6cf6b22467f808d9a
Zoltan Szebenyi <zoltan.szebenyi@rbinternational.com>
2019-05-01 20:23:42 +0200

    fix issue #1 and #3 in windsorize()

--- R/windsorize.R
+++ R/windsorize.R
@@ -3,8 +3,12 @@
 #' Do some windsorization.
 #' @export
 windsorize <- function(x, p = .90) {
-  q <- quantile(x, p)
-  x[x >= q] <- q
+  if(all(is.na(x))) {stop("argument should not be a vector containing only NA-s")}
+  if(all(is.null(x))) {stop("argument should not be a null vector")}
+  q_up <- quantile(x, 0.5 + p / 2 )
+  q_down <- quantile(x, 0.5 - p / 2 )
+  x[x >= q_up] <- q_up
+  x[x <= q_down] <- q_down
   x
 }
 
