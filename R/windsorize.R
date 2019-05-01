4234834ac3b8c51d739bef4b22b7f018089a2a10
Nanda Kallugjeri <49677108+nandakallugjeri@users.noreply.github.com>
2019-05-01 13:40:07 +0200

    Adding error message in case of empty vector or all NA-s vector.

--- R/windsorize.R
+++ R/windsorize.R
@@ -3,8 +3,11 @@
 #' Do some windsorization.
 #' @export
 windsorize <- function(x, p = .90) {
-  q <- quantile(x, probs = c(1-p,p))
-  x[x >= q] <- q
+  if(length(x) == 0) stop('Empty vector passed as an argument.')
+  if( sum(is.na(x)) == length(x) )  stop('A vector of NA-s passed as an argument')
+  q <- quantile(x, probs = c(1-p,p), na.rm = TRUE)
+  x[x >= q[2] ] <- q[2]
+  x[x <= q[1] ] <- q[1]
   x
 }
 
