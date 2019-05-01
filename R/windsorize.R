d8121082c0c40fc5260a35d6f3db906478ff3e0e
Zoltan Szebenyi <zoltan.szebenyi@rbinternational.com>
2019-05-01 20:57:25 +0200

    fix issue #6: updated documentation for windsorize()

--- R/windsorize.R
+++ R/windsorize.R
@@ -1,6 +1,15 @@
 #' Windsorize
-#'
-#' Do some windsorization.
+#' 
+#' Winsorizing or winsorization is the transformation of statistics by limiting
+#' extreme values in the statistical data to reduce the effect of possibly
+#' spurious outliers.
+#' 
+#' @param x A vector.
+#' @param p A quantile.
+#' 
+#' @examples
+#' windsorize(rnorm(5))
+#' 
 #' @export
 windsorize <- function(x, p = .90) {
   if(all(is.na(x))) {stop("argument should not be a vector containing only NA-s")}
