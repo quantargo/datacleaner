de64e384e5ff49fe6168111b8760ea06e9ef3df0
Zoltan Szebenyi <zoltan.szebenyi@rbinternational.com>
2019-05-01 21:11:21 +0200

    update windsorize(): spurious check for null values removed

--- R/windsorize.R
+++ R/windsorize.R
@@ -12,8 +12,7 @@
 #' 
 #' @export
 windsorize <- function(x, p = .90) {
-  if(all(is.na(x))) {stop("argument should not be a vector containing only NA-s")}
-  if(all(is.null(x))) {stop("argument should not be a null vector")}
+  if(all(is.na(x))) {stop("argument should not be a vector containing only NA-s or NULL-s")}
   q_up <- quantile(x, 0.5 + p / 2 )
   q_down <- quantile(x, 0.5 - p / 2 )
   x[x >= q_up] <- q_up
