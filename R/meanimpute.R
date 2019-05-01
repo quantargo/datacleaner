e31b333808393def2bd003536f98187585d7695f
Nanda Kallugjeri <49677108+nandakallugjeri@users.noreply.github.com>
2019-05-01 14:55:00 +0200

    Fixing all the Notes and Warning after Run CMD Check.

--- R/meanimpute.R
+++ R/meanimpute.R
@@ -1,4 +1,8 @@
 #' Meanimputation
+#' 
+#' Removes NA-s with the mean value for the vector \code{x}
+#' 
+#' @param x A numeric vector
 #' @export
 meanimpute <- function(x) {
   x[is.na(x)] <- mean(x, na.rm = TRUE)
