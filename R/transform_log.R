a91d273a75c26fabe2b3804c0b3c7bf75dd41ec2
Nanda Kallugjeri <49677108+nandakallugjeri@users.noreply.github.com>
2019-05-01 14:22:42 +0200

    Adding Documentation for Windsorize function.

--- R/transform_log.R
+++ R/transform_log.R
@@ -1,6 +1,10 @@
 #' Log Transform
 #'  
 #' Transform numerical values into their log values.
+#' @param x A numeric vector.
+#' @return The log values of \code{x}.
+#' @examples
+#' transform_log(exp(rnorm(7)))
 #' @export
  
 transform_log<- function( x )
--- R/windsorize.R
+++ R/windsorize.R
@@ -1,7 +1,15 @@
 #' Windsorize
 #'
-#' Do some windsorization.
+#' 
+#' Replacing values of vector \code{x} greater or smaller then \code{q} quantile values.
+#' 
+#' @param x A numerical vector.
+#' @param p Quantile value for outliers removal.
+#' 
+#' @example 
+#' windsorize(c(1,499,500,501))
 #' @export
+#' 
 windsorize <- function(x, p = .90) {
   if(length(x) == 0) stop('Empty vector passed as an argument.')
   if( sum(is.na(x)) == length(x) )  stop('A vector of NA-s passed as an argument')
