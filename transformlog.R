ef35c5c26493f53730870ddfa7180f465fe45f6b
Vilma Shehu <49863006+VilmaShehu@users.noreply.github.com>
2019-05-04 20:49:52 +0200

    Delete transformlog.R

--- transformlog.R
+++
@@ -1,8 +0,0 @@
-transform_log<- function(x){
-  if (x<0) stop("Negative input not allowed")
-  if (x==0) stop("Input 0 not allowed")
-  if (!is.numeric(x)) stop("Not numeric input not allowed")
-  if (is.null(x)) stop("NULL input not allowed")
-  
-  log(x)
-}
