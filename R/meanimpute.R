93444639c63c50ff8836a3273109b635e6525597
Vilma <vilma.shehu@raiffeisen.al>
2019-05-04 21:40:39 +0200

    Param added

--- R/meanimpute.R
+++ R/meanimpute.R
@@ -1,6 +1,5 @@
 #' Meanimputation
+#' @param x A vector
 #' @export
 meanimpute <- function(x) {
   x[is.na(x)] <- mean(x, na.rm = TRUE)
-  x
-}
--- R/windsorize.R
+++ R/windsorize.R
@@ -3,8 +3,8 @@
 #' Do some windsorization.
 #' @export
 windsorize <- function(x, p = .90) {
-  if (length(x) == 0) stop("Not allowed the use of an empty vector!")
-  if (all(is.na(x)))  stop("Not allowed the use of a vector containing only NA!")
+  if (length(x) == 0){ stop("Not allowed the use of an empty vector!")}
+  if (all(is.na(x))) { stop("Not allowed the use of a vector containing only NA!")}
   
   q <- quantile(x, probs=c(1-p,p))
   x[x >= q] <- q
