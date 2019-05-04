db76d37dd65534cd490dfb2f777448e9dacffef7
Vilma <vilma.shehu@raiffeisen.al>
2019-05-04 21:14:49 +0200

    windsorize() was corrected to do the necessary cheks on its allowed arguments and to consider the smallest value of the vector. This fixes #15 & #16

--- R/windsorize.R
+++ R/windsorize.R
@@ -3,7 +3,10 @@
 #' Do some windsorization.
 #' @export
 windsorize <- function(x, p = .90) {
-  q <- quantile(x, p)
+  if (length(x) == 0) stop("Not allowed the use of an empty vector!")
+  if (all(is.na(x)))  stop("Not allowed the use of a vector containing only NA!")
+  
+  q <- quantile(x, probs=c(1-p,p))
   x[x >= q] <- q
   x
 }
