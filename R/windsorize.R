23a38fe7bbef15218105f5a556df4063c056b256
Andreas Peterseil <andreas.peterseil@rcb.at>
2019-05-05 14:58:42 +0200

    Fix #3: Error is thrown in input is emptry or contains NAs

--- R/windsorize.R
+++ R/windsorize.R
@@ -3,6 +3,7 @@
 #' Do some windsorization.
 #' @export
 windsorize <- function(x, p = .90) {
+  stopifnot(length(x)>0 && sum(is.na(x))==0)
   q <- quantile(x, 0.5 * ( 1 + p * c(-1,1) ) )
   x[x >= q[2] ] <- q[2]
   x[x <= q[1] ] <- q[1] 
