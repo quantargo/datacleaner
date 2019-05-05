d80308d183910bdce2357257ebca23a4964eda99
Andreas Peterseil <andreas.peterseil@rcb.at>
2019-05-05 15:05:21 +0200

    Fix #5: added check of input in transform_log

--- R/transform_log.R
+++ R/transform_log.R
@@ -1,1 +1,10 @@
-transform_log<-function(x) log(x)
+transform_log<-function(x){
+  if(length(x)==0){
+    stop("x must have positive length")
+  }else if(sum(x<=0)>0){
+    stop("x must not contain non-positive values")
+  }else if(sum(is.na(x))>0){
+    stop("x must not contain any NA")
+  } 
+  log(x)
+} 
