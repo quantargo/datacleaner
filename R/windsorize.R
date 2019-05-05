5b39d7d0d627061d3e1e07c30af61abea866129b
Andreas Peterseil <andreas.peterseil@rcb.at>
2019-05-05 14:55:37 +0200

    Fix #1: Included replacement of extremely low values

--- R/windsorize.R
+++ R/windsorize.R
@@ -3,8 +3,9 @@
 #' Do some windsorization.
 #' @export
 windsorize <- function(x, p = .90) {
-  q <- quantile(x, p)
-  x[x >= q] <- q
+  q <- quantile(x, 0.5 * ( 1 + p * c(-1,1) ) )
+  x[x >= q[2] ] <- q[2]
+  x[x <= q[1] ] <- q[1] 
   x
 }
 
