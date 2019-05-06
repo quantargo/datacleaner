d2ed4bac7950a61fb8c1de73caaf55e7aa3dfbdb
marinapopovic11 <50150624+marinapopovic11@users.noreply.github.com>
2019-05-07 00:59:26 +0200

    Update windsorize.R

--- R/windsorize.R
+++ R/windsorize.R
@@ -1,10 +1,25 @@
 #' Windsorize
 #'
-#' Do some windsorization.
+#' Winsorizing or winsorization is the transformation of statistics by limiting 
+#' extreme values to reduce the effect of 
+#' spurious outliers in statistical data.
+#' 
+#' @param x A vector.
+#' @param p A quantile.
+#' 
+#' @examples 
+#' windsorize(rnorm(5))
+#' 
 #' @export
 windsorize <- function(x, p = .90) {
-  q <- quantile(x, p)
-  x[x >= q] <- q
+  if (is.null(x)) {
+    stop("nul vector")}
+    if(is.na(x)){
+      stop("NA vector")}
+  q_low <- quantile(x, 0.5-p/2)
+  q_high <- quantile(x, 0.5+p/2)
+  x[x >= q_high] <- q_high
+  x[x <= q_low] <- q_low
   x
 }
 
